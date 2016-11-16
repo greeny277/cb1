83f96b568d07b96a8eebd6984676f5a2d6871892
Christian Bay <christian.bay@studium.fau.de>
2016-11-16 14:28:52 +0100

    Implement SymbolTable class

---
+++ src/symboltable.py
@@ -0,0 +1,85 @@
+""" This module implements a symboltable. """
+from ast import \
+    Program, \
+    VarDecl, \
+    Function, \
+    Identifier, \
+    Type, \
+    Block, \
+    AssignStmt, \
+    IfStmt, \
+    WhileStmt, \
+    ReturnStmt, \
+    LValue, \
+    IntLiteral, \
+    FloatLiteral, \
+    Operator, \
+    ArithExpr, \
+    FuncCall, \
+    CondExpr
+
+from common import Variable, InternalError
+
+
+class SymbolTable(object):
+    """ Symboltable class """
+    def __init__(self):
+        self.hashTable = {}
+        self.lastEntry = None
+        self.currentDepth = 0
+
+    def enterScope(self):
+        """
+        Enter new Scope. Mark last added Variable.
+        """
+        self.currentDepth = self.currentDepth + 1
+        if self.lastEntry is not None:
+            self.lastEntry.setTos(True)
+
+    def leaveScope(self):
+        """ Leave current Scope. Traverse last added Variables
+        backwards until a Variable which is declared as TopOfStack
+        will be found.
+        """
+        if self.lastEntry is None:
+            return
+
+        while self.lastEntry.isTos() is False:
+            del self.hashTable[self.lastEntry.name]
+            self.lastEntry = self.lastEntry.getPreviuos()
+
+        self.lastEntry.setTos(False)
+        self.currentDepth = self.currentDepth - 1
+
+    def insertVariable(self, variable):
+        """
+        Members:
+        variable: Variable adding to hashtable
+
+        Returns nothing, but adds when possible a
+        new variable to the internal hashtable
+        """
+        checkVar = self.hashTable.get(variable.name)
+        if checkVar is None:
+            self.hashTable[variable.name] = [variable]
+        elif checkVar[0].getDepth() != self.currentDepth:
+            self.hashTable[variable.name] = [variable]+checkVar
+        else:
+            raise InternalError("Double variable declaration in same scope")
+        variable.setDepth(self.currentDepth)
+        variable.setPreviuos(self.lastEntry)
+        self.lastEntry = variable
+
+    def queryVarName(self, name):
+        """
+        Members:
+        name: Identifier of a variable or function
+
+        Return:
+        Variable object declared in most inner scope or None
+        """
+        checkVar = self.hashTable.get(name)
+        if checkVar is None:
+            return None
+        else:
+            return checkVar[0]
