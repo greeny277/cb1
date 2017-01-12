a649ef24328e691f3e59e409205a2927098490f7
Christian Bay <christian.bay@studium.fau.de>
2017-01-12 18:47:24 +0100

    Began implementation of register distribution

---
+++ src/irreg.py
@@ -0,0 +1,63 @@
+import ast
+from ir import \
+        IRProgram, \
+        IRFunction, \
+        IRVariable, \
+        ConstValue, \
+        HardwareRegister, \
+        CLOAD, \
+        CBinary,\
+        CPUSH, \
+        CMUL, \
+        CADD, \
+        CSUB, \
+        CDIV, \
+        CBRA, \
+        CBEQ, \
+        CBNE, \
+        CBGT, \
+        CBGE, \
+        CBLT, \
+        CBLE, \
+        CCALL, \
+        CRET, \
+        CASSGN, \
+        CSTORE
+
+from common import InternalError, Type
+
+
+def irreg(node, hw_registers=None, hw_register=None):
+    if isinstance(node, IRProgram):
+        # Create HW-Registers
+        hw_registers = []
+        # TODO: What is the type here?
+        for name in ['eax', 'ecx', 'edx', 'ebx', 'esi', 'edi']:
+            hw_registers.append(HardwareRegister(name, Type.getIntType()))
+        for x in node.functions:
+            irreg(x, hw_registers)
+    elif isinstance(node, IRFunction):
+        for x in node.instrs():
+            irreg(x, hw_registers)
+
+    elif isinstance(node, IRVariable):
+        # Someone uses a variable, we need to make sure
+        # it is in a real register
+        node.insertBefore(CASSGN(hw_register, node))
+    elif isinstance(node, CBinary):
+        irreg(node.left, None, hw_registers[0])
+        irreg(node.right, None, hw_registers[1])
+        if isinstance(node, CMUL):
+            node.insertBefore(CMUL(hw_registers[0], hw_registers[1]))
+        elif isinstance(node, CADD):
+            node.insertBefore(CADD(hw_registers[0], hw_registers[1]))
+        elif isinstance(node, CSUB):
+            node.insertBefore(CSUB(hw_registers[0], hw_registers[1]))
+        elif isinstance(node, CDIV):
+            node.insertBefore(CDIV(hw_registers[0], hw_registers[1]))
+
+        #TODO: Delete old airthmetic expression
+
+    else:
+        for x in node.children():
+            irreg(x, hw_registers)
