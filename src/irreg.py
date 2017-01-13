a07989b81b66fec9e079835a483ce26b27f66a1c
Christian Bay <christian.bay@studium.fau.de>
2017-01-13 11:18:48 +0100

    Ex05: Remove older assgnmt node

--- src/irreg.py
+++ src/irreg.py
@@ -31,7 +31,6 @@ def irreg(node, hw_registers=None, hw_register=None):
     if isinstance(node, IRProgram):
         # Create HW-Registers
         hw_registers = {}
-        # TODO: What is the type here?
         for name in ['rax', 'rcx', 'rdx', 'rbx', 'rsi', 'rdi']:
             hw_registers[name] = HardwareRegister(name, Type.getIntType())
         for x in node.functions:
@@ -48,15 +47,15 @@ def irreg(node, hw_registers=None, hw_register=None):
         irreg(node.left, None, hw_registers['rax'])
         irreg(node.right, None, hw_registers['rcx'])
         if isinstance(node, CMUL):
-            node.insertBefore(CMUL(hw_registers['rax'], hw_registers['rcx']))
+            node.insertBefore(CMUL(hw_registers['rax'], hw_registers['rax'], hw_registers['rcx']))
         elif isinstance(node, CADD):
-            node.insertBefore(CADD(hw_registers['rax'], hw_registers['rcx']))
+            node.insertBefore(CADD(hw_registers['rax'], hw_registers['rax'], hw_registers['rcx']))
         elif isinstance(node, CSUB):
-            node.insertBefore(CSUB(hw_registers['rax'], hw_registers['rcx']))
+            node.insertBefore(CSUB(hw_registers['rax'], hw_registers['rax'], hw_registers['rcx']))
         elif isinstance(node, CDIV):
-            node.insertBefore(CDIV(hw_registers['rax'], hw_registers['rcx']))
-
-        #TODO: Delete old airthmetic expression
+            node.insertBefore(CDIV(hw_registers['rax'], hw_registers['rax'], hw_registers['rcx']))
+        node.insertBefore(CASSGN(node.target, hw_registers['rax']))
+        node.remove()
 
     else:
         for x in node.children():
